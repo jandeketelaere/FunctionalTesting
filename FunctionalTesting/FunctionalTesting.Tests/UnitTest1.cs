d239d2df695eb30f771daeff3bc8d0baf2f220c9
Jan Deketelaere <jdeket@hotmail.com>
2016-08-07 21:37:36 +0200

    Playing around with Result class

---
+++ FunctionalTesting/FunctionalTesting.Tests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FunctionalTesting.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            GetContact()
+                //.OnSuccessAndWhen(
+                //    c => c.Id == -1,
+                //    () => CreateContact(),
+                //    () => Log("No contact was created"));
+
+                .OnSuccess(c => c.Id == -1 ? CreateContact().Cast() : Log("No contact was created"))
+                .OnSuccess(CreateMsisdn)
+                .When(r => r.IsFailure, r =>
+                {
+                    throw new Exception($"An exception has occured in API {r.ApiCall}, error code {r.ErrorCode}");
+                });
+
+
+            //.OnSuccess(c => c.Id == -1, () => CreateContact());
+
+        }
+
+        private static ApiResult<Contact> GetContact()
+        {
+            var contact = new Contact {Id = -1};
+
+            return ApiResult.Ok("GetContact", contact);
+
+            //return ApiResult.Fail<Contact>("GetContact", "6");
+        }
+
+        private static ApiResult<Contact> CreateContact()
+        {
+            var contact = new Contact { Id = 1, Name = "NewContact" };
+
+            return ApiResult.Ok("CreateContact", contact);
+        }
+
+        private static ApiResult Log(string message)
+        {
+            return ApiResult.Ok("Log");
+        }
+
+        private static ApiResult CreateMsisdn()
+        {
+            //return ApiResult.Ok("CreateMsisdn");
+
+            return ApiResult.Fail("CreateMsisdn", "2");
+        } 
+
+        private class Contact
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ FunctionalTesting/FunctionalTesting/ApiResultExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalTesting
+{
+    public static class ApiResultExtensions
+    {
+        public static ApiResult OnSuccessAndWhen<T>(this ApiResult<T> result, Func<T, bool> predicate,
+            Func<ApiResult> trueResult, Func<ApiResult> falseResult)
+        {
+            if (result.IsFailure) return ApiResult.Fail(result.ApiCall, result.ErrorCode);
+
+            return predicate(result.Value) ? trueResult() : falseResult();
+        }
+
+        public static ApiResult<T> OnSuccessAndWhen<T>(this ApiResult<T> result, Func<T, bool> predicate,
+            Func<ApiResult<T>> trueResult, Func<ApiResult<T>> falseResult)
+        {
+            if (result.IsFailure) return ApiResult.Fail<T>(result.ApiCall, result.ErrorCode);
+
+            return predicate(result.Value) ? trueResult() : falseResult();
+        }
+
+        public static ApiResult OnSuccess<T>(this ApiResult<T> result, Func<T, ApiResult> func)
+        {
+            if (result.IsFailure) return ApiResult.Fail(result.ApiCall, result.ErrorCode);
+
+            return func(result.Value);
+        }
+
+        //public static ApiResult<T> OnSuccess<T>(this ApiResult<T> result, Func<T, ApiResult<T>> func)
+        //{
+        //    if (result.IsFailure) return ApiResult.Fail<T>(result.ApiCall, result.ErrorCode);
+
+        //    return func(result.Value);
+        //}
+
+        public static ApiResult OnSuccess(this ApiResult result, Func<ApiResult> func)
+        {
+            if (result.IsFailure) return ApiResult.Fail(result.ApiCall, result.ErrorCode);
+
+            return func();
+        }
+
+        //public static ApiResult<T> OnSuccess<T>(this ApiResult<T> result, Func<T, bool> predicate, Func<ApiResult<T>> func)
+        //{
+        //    if (result.IsFailure) return ApiResult.Fail<T>(result.ApiCall, result.ErrorCode);
+
+        //    return predicate(result.Value) ? func() : result;
+        //}
+
+        public static void When(this ApiResult result, Func<ApiResult, bool> predicate, Action<ApiResult> action)
+        {
+            if (predicate(result)) action(result);
+        }
+
+        public static ApiResult Cast<T>(this ApiResult<T> result)
+        {
+            return result.IsSuccessful ? ApiResult.Ok(result.ApiCall) : ApiResult.Fail(result.ApiCall, result.ErrorCode);
+        }
+    }
+}
