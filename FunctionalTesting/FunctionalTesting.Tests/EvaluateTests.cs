f163720676ae723f45b5feb3ebf4bfc901219e68
Jan Deketelaere <jdeket@hotmail.com>
2017-03-15 20:54:54 +0100

    Added Evaluate method

---
+++ FunctionalTesting/FunctionalTesting.Tests/EvaluateTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static FunctionalTesting.EvaluateMethods;
+
+namespace FunctionalTesting.Tests
+{
+    [TestClass]
+    public class EvaluateTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Evaluate(1, 2, 3)
+                .When(
+                    i => i == 1,
+                    i => i < 5,
+                    i => i < 6
+                    )
+                    .Then(PrintYay)
+                    .Else(PrintNay)
+                .When(
+                    i => i == 1,
+                    i => i < 5,
+                    i => i < 6
+                    )
+                    .Then(PrintYay)
+                    .Else(PrintNay)
+                .When(
+                    i => i > 0,
+                    i => i <= 2,
+                    i => i == 3
+                    )
+                    .Then(PrintYay)
+                .When(
+                    i => i >= 0,
+                    i => i == 2
+                    )
+                    .Then(PrintYay)
+                ;
+        }
+
+        private static void PrintYay()
+        {
+            Console.WriteLine("Yay!");
+        }
+
+        private static void PrintNay()
+        {
+            Console.WriteLine("Nay!");
+        }
+    }
+}
---
+++ FunctionalTesting/FunctionalTesting/Evaluate.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalTesting
+{
+    public class Evaluate<T>
+    {
+        private readonly List<T> _values;
+
+        public Evaluate(List<T> values)
+        {
+            _values = values;
+        }
+
+        public Evaluate(params T[] values)
+        {
+            _values = new List<T>(values);
+        }
+
+        public WhenEvaluate<T> When(params Func<T, bool>[] predicates)
+        {
+            return _values.Where((t, i) => predicates.ElementAtOrDefault(i) != null && !predicates[i](t)).Any()
+                ? new WhenEvaluate<T>(this, false)
+                : new WhenEvaluate<T>(this, true);
+        }
+    }
+
+    public class WhenEvaluate<T>
+    {
+        private readonly Evaluate<T> _evaluate;
+        private readonly bool _conditionsMet;
+
+        public WhenEvaluate(Evaluate<T> evaluate, bool conditionsMet)
+        {
+            _evaluate = evaluate;
+            _conditionsMet = conditionsMet;
+        } 
+
+        public ThenEvaluate<T> Then(Action action)
+        {
+            if (_conditionsMet) action();
+
+            return new ThenEvaluate<T>(_evaluate, _conditionsMet);
+        }
+    }
+
+    public class ThenEvaluate<T>
+    {
+        private readonly Evaluate<T> _evaluate;
+        private readonly bool _conditionsMet;
+
+        public ThenEvaluate(Evaluate<T> evaluate, bool conditionsMet)
+        {
+            _evaluate = evaluate;
+            _conditionsMet = conditionsMet;
+        }
+
+        public Evaluate<T> Else(Action action)
+        {
+            if (!_conditionsMet) action();
+
+            return _evaluate;
+        }
+
+        public WhenEvaluate<T> When(params Func<T, bool>[] predicates)
+        {
+            return _evaluate.When(predicates);
+        }
+    }
+
+    public static class EvaluateMethods
+    {
+        public static Evaluate<T> Evaluate<T>(params T[] values)
+        {
+            return new Evaluate<T>(values);
+        }
+    }
+}
